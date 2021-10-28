6d969d10b633cbd1be819c6fb6616108025843d2
Lukasz Feldman <lukasz.feldman@oracle.com>
2021-10-28 19:19:53 +0200

    updated deploy_pipeline.tf

--- deploy_pipeline.tf
+++ deploy_pipeline.tf
@@ -6,7 +6,7 @@ resource "oci_devops_deploy_environment" "test_environment" {
   description             = "oke based enviroment"
   deploy_environment_type = "OKE_CLUSTER"
   project_id              = oci_devops_project.test_project.id
-  cluster_id              = module.oci-oke[0].cluster.id
+  cluster_id              = var.create_new_oke_cluster ? module.oci-oke[0].cluster.id : var.existent_oke_cluster_id
   defined_tags            = { "${oci_identity_tag_namespace.ArchitectureCenterTagNamespace.name}.${oci_identity_tag.ArchitectureCenterTag.name}" = var.release }
 }
 
