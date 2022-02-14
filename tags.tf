d304fde64f70c99adf064dd7a013b03259c82df9
lfeldman <lukasz.feldman@oracle.com>
2022-02-14 15:26:14 +0100

    Added OKE tagging

--- tags.tf
+++ tags.tf
@@ -24,7 +24,7 @@ resource "oci_identity_tag" "ArchitectureCenterTag" {
 
   validator {
     validator_type = "ENUM"
-    values         = ["release", "1.0.2"]
+    values         = ["release", "1.0.3"]
   }
 
   provisioner "local-exec" {
